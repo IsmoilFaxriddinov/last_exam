5cc06dae5abbaecc567aaf72cf9145f791da04f9
Ismoil <ismoilfaxriddinov2010@gmail.com>
2025-03-08 13:38:31 +0500

    code

--- conf/urls.py
+++ conf/urls.py
@@ -49,8 +49,3 @@ urlpatterns += [
    path('docs/', schema_view.with_ui('swagger', cache_timeout=0), name='schema-swagger-ui'),
    path('redoc/', schema_view.with_ui('redoc', cache_timeout=0), name='schema-redoc'),
 ]
-
-urlpatterns += [
-    path('admin/', admin.site.urls),
-    path('api/v1/posts/', include('admins.urls', namespace='admins')),
-]
