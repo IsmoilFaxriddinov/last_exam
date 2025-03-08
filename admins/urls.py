421d5cccc01824f4ac30119fa6bb095522eeb36f
Ismoil <ismoilfaxriddinov2010@gmail.com>
2025-03-08 13:44:17 +0500

    admin

--- admins/urls.py
+++ admins/urls.py
@@ -1,7 +1,9 @@
 from django.urls import path
 
+from admins.views import test_view
+
 app_name = 'admins'
 
 urlpatterns = [
-    # path('', ..., 'admin')
+    path('admin/', test_view, name='admin'),
 ]
