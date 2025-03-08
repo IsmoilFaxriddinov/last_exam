1e29a18d3e2a5274254dc8d78fd366f2d3bbbe6b
Ismoil <ismoilfaxriddinov2010@gmail.com>
2025-03-08 13:35:32 +0500

    drf yasg

--- conf/settings.py
+++ conf/settings.py
@@ -39,7 +39,13 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+
+    'drf_yasg',
     'rest_framework',
+    'rest_framework_simplejwt',
+    'rest_framework_simplejwt.token_blacklist',
+    'corsheaders',
+
     'app_common',
     'admins',
     'teachers',
--- conf/urls.py
+++ conf/urls.py
@@ -16,10 +16,41 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import include, path
+from rest_framework import permissions
+from drf_yasg.views import get_schema_view
+from drf_yasg import openapi
+from rest_framework_simplejwt.authentication import JWTAuthentication
+
+
+schema_view = get_schema_view(
+    openapi.Info(
+        title="N55 medium clone API",
+        default_version="v1",
+        description="Class project in Ismoil",
+        terms_of_service="https://www.google.com/policies/terms/",
+        contact=openapi.Contact(email="ismoilfaxriddinov2010@gmail.com"),
+        license=openapi.License(name="BSD License"),
+    ),
+    public=True,
+    permission_classes=[permissions.AllowAny],
+    authentication_classes=[(JWTAuthentication,)],
+)
+
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path("admins/", include('admins.urls'), name="admins"),
-    path("teachers/", include('teachers.urls'), name="teachers"),
-    path("students/", include('students.urls'), name="students"),
+    # path("teachers/", include('teachers.urls'), name="teachers"),
+    # path("students/", include('students.urls'), name="students"),
+]
+
+urlpatterns += [
+   path('swagger<format>/', schema_view.without_ui(cache_timeout=0), name='schema-json'),
+   path('docs/', schema_view.with_ui('swagger', cache_timeout=0), name='schema-swagger-ui'),
+   path('redoc/', schema_view.with_ui('redoc', cache_timeout=0), name='schema-redoc'),
+]
+
+urlpatterns += [
+    path('admin/', admin.site.urls),
+    path('api/v1/posts/', include('admins.urls', namespace='admins')),
 ]
