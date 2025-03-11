38d317a8bde57a8e85f93adc504c4ea1e725450c
Ismoil <ismoilfaxriddinov2010@gmail.com>
2025-03-11 12:56:11 +0500

    admin

--- admins/urls.py
+++ admins/urls.py
@@ -1,12 +1,21 @@
 from django.urls import path, include
+from dotenv import load_dotenv
 from rest_framework.routers import DefaultRouter
-from .views import SuperAdminViewSet, AdminViewSet, GroupViewSet
+from rest_framework_simplejwt.views import TokenObtainPairView, TokenRefreshView
+from admins.views import SuperAdminViewSet, AdminViewSet, GroupViewSet
+from students.views import StudentViewSet
+from teachers.views import TeacherViewSet, HomeworkViewSet
 
 router = DefaultRouter()
 router.register(r'superadmins', SuperAdminViewSet)
 router.register(r'admins', AdminViewSet)
 router.register(r'groups', GroupViewSet)
+router.register(r'students', StudentViewSet)
+router.register(r'teachers', TeacherViewSet)
+router.register(r'homeworks', HomeworkViewSet)
 
 urlpatterns = [
     path('', include(router.urls)),
+    path('token/', TokenObtainPairView.as_view(), name='token_obtain_pair'),
+    path('token/refresh/', TokenRefreshView.as_view(), name='token_refresh'),
 ]
