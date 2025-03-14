83ac3b6c96b0bbfed10eabbaf6d0ef8d4a331255
Ismoil <ismoilfaxriddinov2010@gmail.com>
2025-03-14 23:34:59 +0500

    add groups

--- admins/views.py
+++ admins/views.py
@@ -43,21 +43,12 @@ class GroupViewSet(viewsets.ModelViewSet):
     def perform_create(self, serializer):
         print("Authenticated User:", self.request.user)
         print("Is Authenticated:", self.request.user.is_authenticated)
-
+        
         admin = AdminModel.objects.filter(user=self.request.user).first()
         if not admin:
-            return Response(
-                {"error": "User is not associated with an AdminModel"},
-                status=status.HTTP_403_FORBIDDEN
-            )
-
-        # Ma'lumotlarni saqlash
+            return Response({"error": "User is not associated with an AdminModel"},status=status.HTTP_403_FORBIDDEN)
         group = serializer.save(admin=admin)
-
-        # Muvaffaqiyatli natija qaytarish
         return Response(
-            {"success": True, "message": "Group created successfully!", "group_id": group.id, "group_name": group.name,
-            },
-            status=status.HTTP_201_CREATED
+            {"success": True, "message": "Group created successfully!", "group_id": group.id, "group_name": group.name,},status=status.HTTP_201_CREATED
         )
 
