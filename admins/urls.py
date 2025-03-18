32208ecac8ed38bc8011124590377a062d64181f
Ismoil <ismoilfaxriddinov2010@gmail.com>
2025-03-18 16:40:07 +0500

    last commit

--- admins/urls.py
+++ admins/urls.py
@@ -2,16 +2,11 @@ from django.urls import path, include
 from rest_framework.routers import DefaultRouter
 from rest_framework_simplejwt.views import TokenObtainPairView, TokenRefreshView
 from admins.views import SuperAdminViewSet, AdminViewSet, GroupViewSet
-from students.views import StudentViewSet
-from teachers.views import TeacherViewSet, HomeworkViewSet
 
 router = DefaultRouter()
 router.register(r'superadmins', SuperAdminViewSet)
 router.register(r'admins', AdminViewSet)
 router.register(r'groups', GroupViewSet)
-router.register(r'students', StudentViewSet)
-router.register(r'teachers', TeacherViewSet)
-router.register(r'homeworks', HomeworkViewSet)
 
 urlpatterns = [
     path('', include(router.urls)),
